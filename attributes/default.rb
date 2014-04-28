66f18467fc296f38fd9b322746a13d14b5329a0d
Koji Tanaka <kj.tanaka@gmail.com>
2014-04-27 20:24:05 -0400

    adding attributes

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['openstack']['keystone_db'] = "keystone"
+default['openstack']['glance_db'] = "glance"
+default['openstack']['cinder_db'] = "cinder"
+default['openstack']['nova_db'] = "nova"
